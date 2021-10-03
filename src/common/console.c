8f7ffb9f3e5351358edae371383690ba9bb35355
Stijn Rogiest <reddusted@gmail.com>
2021-10-03 15:47:52 +0200

    Console functions in seperate file

---
+++ src/common/console.c
@@ -0,0 +1,79 @@
+#define CONSOLE_COLOR unsigned char
+#define CONSOLE_COLOR_BLACK (CONSOLE_COLOR)0x0
+#define CONSOLE_COLOR_BLUE (CONSOLE_COLOR)0x1
+#define CONSOLE_COLOR_GREEN (CONSOLE_COLOR)0x2
+#define CONSOLE_COLOR_CYAN (CONSOLE_COLOR)0x3
+#define CONSOLE_COLOR_RED (CONSOLE_COLOR)0x4
+#define CONSOLE_COLOR_PURPLE (CONSOLE_COLOR)0x5
+#define CONSOLE_COLOR_BROWN (CONSOLE_COLOR)0x6
+#define CONSOLE_COLOR_GRAY (CONSOLE_COLOR)0x7
+#define CONSOLE_COLOR_DARK_GRAY (CONSOLE_COLOR)0x8
+#define CONSOLE_COLOR_LIGHT_BLUE (CONSOLE_COLOR)0x9
+#define CONSOLE_COLOR_LIGHT_GREEN (CONSOLE_COLOR)0xA
+#define CONSOLE_COLOR_LIGHT_CYAN (CONSOLE_COLOR)0xB
+#define CONSOLE_COLOR_LIGHT_RED (CONSOLE_COLOR)0xC
+#define CONSOLE_COLOR_LIGHT_PURPLE (CONSOLE_COLOR)0xD
+#define CONSOLE_COLOR_YELLOW (CONSOLE_COLOR)0xE
+#define CONSOLE_COLOR_WHITE (CONSOLE_COLOR)0xF
+
+struct VideoChar
+{
+    char charater;
+    unsigned char color;
+};
+
+volatile struct VideoChar *videoMemory = (struct VideoChar *)0xb8000;
+
+unsigned char currentConsoleColor = 0x0f;
+unsigned int x = 0, y = 0, w = 80, h = 25;
+
+void console_set_color(CONSOLE_COLOR foreground, CONSOLE_COLOR background)
+{
+    currentConsoleColor = foreground | (background << 4);
+}
+
+void console_print_char(char c)
+{
+    if (c == '\n')
+    {
+        y++;
+        x = 0;
+    }
+    else
+    {
+        videoMemory[x + y * w].charater = c;
+        videoMemory[x + y * w].color = currentConsoleColor;
+        if (++x >= w)
+        {
+            y++;
+            x = 0;
+        }
+    }
+}
+
+void set_cursor(unsigned int newX, unsigned int newY)
+{
+    x = newX;
+    y = newY;
+}
+
+void console_clear()
+{
+    for (int ix = 0; ix < w; ix++)
+    {
+        for (int iy = 0; iy < h; iy++)
+        {
+            videoMemory[ix + iy * w].charater = 0x0;
+            videoMemory[ix + iy * w].color = 0x0;
+        }
+    }
+}
+
+void console_print(char *str)
+{
+    while (*str)
+    {
+        console_print_char(*str);
+        str++;
+    }
+}
--- src/common/main.c
+++ src/common/main.c
@@ -1,13 +1,4 @@
-struct VideoChar
-{
-    char charater;
-    unsigned char color;
-};
-
-volatile struct VideoChar *videoMemory = (struct VideoChar *)0xb8000;
-
-unsigned char printColor = 0x0f;
-unsigned int x = 0, y = 0, w = 80, h = 25;
+#include "console.c"
 
 // #define u8 unsigned char
 // #define i8 char
@@ -18,78 +9,15 @@ unsigned int x = 0, y = 0, w = 80, h = 25;
 // #define i64 long long
 // #define u64 unsigned long long
 
-#define COLOR unsigned char
-#define COLOR_BLACK (COLOR)0x0
-#define COLOR_BLUE (COLOR)0x1
-#define COLOR_GREEN (COLOR)0x2
-#define COLOR_CYAN (COLOR)0x3
-#define COLOR_RED (COLOR)0x4
-#define COLOR_PURPLE (COLOR)0x5
-#define COLOR_BROWN (COLOR)0x6
-#define COLOR_GRAY (COLOR)0x7
-#define COLOR_DARK_GRAY (COLOR)0x8
-#define COLOR_LIGHT_BLUE (COLOR)0x9
-#define COLOR_LIGHT_GREEN (COLOR)0xA
-#define COLOR_LIGHT_CYAN (COLOR)0xB
-#define COLOR_LIGHT_RED (COLOR)0xC
-#define COLOR_LIGHT_PURPLE (COLOR)0xD
-#define COLOR_YELLOW (COLOR)0xE
-#define COLOR_WHITE (COLOR)0xF
-
-void set_console_color(COLOR foreground, COLOR background)
-{
-    printColor = foreground | (background << 4);
-}
-
-void print_char(char c)
-{
-    if (c == '\n')
-    {
-        y++;
-        x = 0;
-    }
-    else
-    {
-        videoMemory[x + y * w].charater = c;
-        videoMemory[x + y * w].color = printColor;
-        if (++x >= w)
-        {
-            y++;
-            x = 0;
-        }
-    }
-}
-
-void set_cursor(unsigned int newX, unsigned int newY)
-{
-    x = newX;
-    y = newY;
-}
-
-void clear()
-{
-    for (int ix = 0; ix < w; ix++)
-    {
-        for (int iy = 0; iy < h; iy++)
-        {
-            videoMemory[ix + iy * w].charater = 0x0;
-            videoMemory[ix + iy * w].color = 0x0;
-        }
-    }
-}
-
-void print(char *str)
-{
-    while (*str)
-    {
-        print_char(*str);
-        str++;
-    }
-}
-
 int kernel_main()
 {
-    clear();
-    set_console_color(COLOR_WHITE, COLOR_BLACK);
-    print("booting...");
+    console_clear();
+    console_set_color(CONSOLE_COLOR_WHITE, CONSOLE_COLOR_BLACK);
+    console_print("booting...\n");
+
+    console_set_color(CONSOLE_COLOR_WHITE, CONSOLE_COLOR_CYAN);
+    for (int i = 0; i < 256; i++)
+    {
+        console_print_char(i);
+    }
 }
