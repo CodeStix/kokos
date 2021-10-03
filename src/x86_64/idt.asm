7c5e87abd2d5550ed9a5b148b04174100c2abfb6
Stijn Rogiest <reddusted@gmail.com>
2021-10-03 23:09:26 +0200

    Show register contents when hitting breakpoint

--- src/x86_64/idt.asm
+++ src/x86_64/idt.asm
@@ -1,18 +1,106 @@
 extern console_print
+extern console_print_u64
+extern console_print_char
 
 section .text
 bits 64
 
+print_registers:
+    push rdi
+    push rsi
+    push rdi
+    push rsi
+
+    mov rdi, info_rax
+    call console_print
+    mov rdi, rax
+    mov rsi, 10
+    call console_print_u64
+
+    mov rdi, info_rbx
+    call console_print
+    mov rdi, rbx
+    mov rsi, 10
+    call console_print_u64
+
+    mov rdi, info_rcx
+    call console_print
+    mov rdi, rcx
+    mov rsi, 10
+    call console_print_u64
+
+    mov rdi, info_rdx
+    call console_print
+    mov rdi, rdx
+    mov rsi, 10
+    call console_print_u64
+
+    mov rdi, info_rsi
+    call console_print
+    pop rdi
+    mov rsi, 10
+    call console_print_u64
+
+    mov rdi, info_rdi
+    call console_print
+    pop rdi
+    mov rsi, 10
+    call console_print_u64
+
+    mov rdi, info_ebp
+    call console_print
+    mov rdi, rbp
+    mov rsi, 10
+    call console_print_u64
+
+    mov rdi, info_esp
+    call console_print
+    mov rdi, rsp
+    mov rsi, 10
+    call console_print_u64
+
+    mov rdi, 0xA
+    call console_print_char
+
+    pop rsi
+    pop rdi
+    ret
+
 interrupt_handle_divide_zero:
+    push rdi
     mov rdi, error_divide_by_zero
     call console_print  ; Notes on calling c functions from assembly (using System V X86_64 calling convention): https://wiki.osdev.org/Calling_Conventions
+    pop rdi
     hlt
-    ; iretq
 
 interrupt_handle_breakpoint:
+    push rdi
     mov rdi, error_breakpoint
     call console_print  
+    call print_registers
+    pop rdi
     iretq
+
+interrupt_handle_overflow:
+    push rdi
+    mov rdi, error_overflow
+    call console_print  
+    pop rdi
+    iretq
+
+interrupt_handle_bound_range:
+    push rdi
+    mov rdi, error_bound_range
+    call console_print  
+    pop rdi
+    hlt
+
+interrupt_handle_invalid_opcode:
+    push rdi
+    mov rdi, error_invalid_opcode
+    call console_print  
+    pop rdi
+    hlt
 
 %macro register_interrupt_handler 2             
     mov rax, %1
@@ -32,6 +120,9 @@ interrupt_handle_breakpoint:
 load_idt:
     register_interrupt_handler 0, interrupt_handle_divide_zero
     register_interrupt_handler 3, interrupt_handle_breakpoint
+    register_interrupt_handler 4, interrupt_handle_overflow
+    register_interrupt_handler 5, interrupt_handle_bound_range
+    register_interrupt_handler 6, interrupt_handle_invalid_opcode
     lidt [idt64.pointer]
     ret
 
@@ -42,10 +133,34 @@ error_divide_by_zero:
     db "error: divide by zero!", 0xA, 0
 error_breakpoint:
     db "warning: hit breakpoint!", 0xA, 0
+error_overflow:
+    db "error: overflow!", 0xA, 0
+error_bound_range:
+    db "error: bound range!", 0xA, 0
+error_invalid_opcode:
+    db "error: invalid opcode!", 0xA, 0
+info_registers:
+    db "registers", 0
+
+info_rax:
+    db "rax=", 0
+info_rbx:
+    db ", rbx=", 0
+info_rcx:
+    db ", rcx=", 0
+info_rdx:
+    db ", rdx=", 0
+info_rdi:
+    db ", rdi=", 0
+info_rsi:
+    db ", rsi=", 0
+info_esp:
+    db ", rsp=", 0
+info_ebp:
+    db ", rbp=", 0
 
 idt64:
     resb 16 * 256 - ($ - idt64) ; Reserve the whole idt, each entry is 16 bytes
-
 .pointer:
     dw $ - idt64 - 1
     dq idt64
