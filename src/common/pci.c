baa5a50ce53a7a38b50d9f1d2e585a5cb7acc214
Stijn Rogiest <reddusted@gmail.com>
2021-10-08 22:32:41 +0200

    pci_class_string function

--- src/common/pci.c
+++ src/common/pci.c
@@ -35,25 +35,33 @@ void pci_scan()
                     unsigned char header_type = pci_config_read8(bus, slot, function, 0xe) & 0b01111111;
                     unsigned char multifunction = pci_config_read8(bus, slot, function, 0xe) >> 7;
 
-                    console_print("pci device at bus=");
+                    console_print("pci device at\n bus=");
                     console_print_u32(bus, 10);
-                    console_print(", slot=");
+                    console_print(" slot=");
                     console_print_u32(slot, 10);
-                    console_print(", function=");
+                    console_print(" function=");
                     console_print_u32(function, 10);
-                    console_print(", vendor=0x");
+                    console_print(" vendor=0x");
                     console_print_u32(vendor, 16);
-                    console_print(", device=0x");
+                    console_print(" device=0x");
                     console_print_u32(device, 16);
-                    console_print(", class=0x");
+                    console_new_line();
+
+                    console_print(" class=0x");
                     console_print_u32(class, 16);
-                    console_print(", subclass=0x");
+                    console_print_char(' ');
+                    console_print(pci_class_string(class));
+                    console_print(" subclass=0x");
                     console_print_u32(subclass, 16);
-                    console_print(", header_type=0x");
+                    console_print_char(' ');
+                    console_print(pci_subclass_string(class, subclass));
+                    console_new_line();
+
+                    console_print(" header_type=0x");
                     console_print_u32(header_type, 16);
                     console_print(", multifunction=");
                     console_print_u32(multifunction, 10);
-                    console_print("\n");
+                    console_new_line();
 
                     if (!multifunction)
                     {
@@ -63,4 +71,67 @@ void pci_scan()
             }
         }
     }
+}
+
+char *pci_class_string(unsigned char class_code)
+{
+    // Extracted from https://wiki.osdev.org/PCI
+    switch (class_code)
+    {
+    case 0x00:
+        return "Unclassified";
+    case 0x01:
+        return "Mass Storage Controller";
+    case 0x02:
+        return "Network Controller";
+    case 0x03:
+        return "Display Controller";
+    case 0x04:
+        return "Multimedia Controller";
+    case 0x05:
+        return "Memory Controller";
+    case 0x06:
+        return "Bridge";
+    case 0x07:
+        return "Simple Communication Controller";
+    case 0x08:
+        return "Base System Peripheral";
+    case 0x09:
+        return "Input Device Controller";
+    case 0x0A:
+        return "Docking Station";
+    case 0x0B:
+        return "Processor";
+    case 0x0C:
+        return "Serial Bus Controller";
+    case 0x0D:
+        return "Wireless Controller";
+    case 0x0E:
+        return "Intelligent Controller";
+    case 0x0F:
+        return "Satellite Communication Controller";
+    case 0x10:
+        return "Encryption Controller";
+    case 0x11:
+        return "Signal Processing Controller";
+    case 0x12:
+        return "Processing Accelerator";
+    case 0x13:
+        return "Non-Essential Instrumentation";
+    case 0x14:
+        return "0x3F (Reserved)";
+    case 0x40:
+        return "Co-Processor";
+    case 0x41:
+        return "0xFE (Reserved)";
+    default:
+    case 0xFF:
+        return "Unassigned Class";
+    }
+}
+
+char *pci_subclass_string(unsigned char class_code, unsigned char subclass_code)
+{
+    // TODO extract from https://wiki.osdev.org/PCI
+    return pci_class_string(class_code);
 }
--- src/include/pci.h
+++ src/include/pci.h
@@ -9,4 +9,10 @@ unsigned short pci_config_read16(int bus, int slot, int function, int offset);
 // Reads a 8 bit field from pci configuration space at bus, slot, function, offset
 unsigned char pci_config_read8(int bus, int slot, int function, int offset);
 
+// Gets a string represenstation of a pci class code
+char *pci_class_string(unsigned char class_code);
+
+// Gets a specific string represenstation of pci class codes
+char *pci_subclass_string(unsigned char class_code, unsigned char subclass_code);
+
 void pci_scan();
