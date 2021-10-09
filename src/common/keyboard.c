bd2c910607feeba0987c1222248da2dab3f732a6
Stijn Rogiest <reddusted@gmail.com>
2021-10-09 13:40:46 +0200

    Test ps2 ports

--- src/common/keyboard.c
+++ src/common/keyboard.c
@@ -45,61 +45,84 @@ void keyboard_init()
 
     // 0x64 is the ps2 command port
     // Send 'Disable first PS/2 port'
-    port_out8(0x64, 0xAD);
+    port_out8(KEYBOARD_COMMAND_REGISTER, 0xAD);
 
     keyboard_wait_can_write();
 
     // Send 'Disable second PS/2 port', will be ignored if it only has one
-    port_out8(0x64, 0xA7);
+    port_out8(KEYBOARD_COMMAND_REGISTER, 0xA7);
 
     // Remove previous data that may have been in the controller's buffer
     keyboard_clear_buffer();
 
     // Send 'Read byte 0 from internal RAM' command, this will contain the controller's configuration byte
-    port_out8(0x64, 0x20);
+    port_out8(KEYBOARD_COMMAND_REGISTER, 0x20);
 
     // Wait for response
     keyboard_wait_can_read();
 
     // Read from data register, which will now contain the configuration byte
-    unsigned char config = port_in8(0x60);
+    unsigned char config = port_in8(KEYBOARD_DATA_REGISTER);
 
     // Disable interrupts and port translation
     config = config & ~0b01000011;
-
-    console_print("config byte = 0x");
-    console_print_u32(config, 2);
-    console_new_line();
 
     // Wait for the controller to be ready to receive data
     keyboard_wait_can_write();
 
     // Write the new config to the controller using the 'Write next byte to "byte 0" of internal RAM' command
-    port_out8(0x64, 0x60);
+    port_out8(KEYBOARD_COMMAND_REGISTER, 0x60);
 
     keyboard_wait_can_write();
 
     // Place the new config in the data port
-    port_out8(0x60, config);
+    port_out8(KEYBOARD_DATA_REGISTER, config);
 
     keyboard_wait_can_write();
 
     // Send 'Test controller' command
-    port_out8(0x64, 0xAA);
+    port_out8(KEYBOARD_COMMAND_REGISTER, 0xAA);
 
-    // Wait for response
     keyboard_wait_can_read();
 
-    // Read from data register
-    unsigned char result = port_in8(0x60);
-    if (result == 0x55)
+    unsigned char controller_test_result = port_in8(KEYBOARD_DATA_REGISTER);
+    if (controller_test_result != 0x55)
     {
-        console_print("test passed\n");
+        console_print("ps2 controller self test failed!\n");
+        return;
     }
-    else
+
+    keyboard_wait_can_write();
+
+    // Test ps2 port 1
+    port_out8(KEYBOARD_COMMAND_REGISTER, 0xAB);
+
+    keyboard_wait_can_read();
+
+    unsigned char port1_test_result = port_in8(KEYBOARD_DATA_REGISTER);
+    if (port1_test_result != 0)
     {
-        console_print("test failed: ");
-        console_print_u32(result, 10);
-        console_new_line();
+        console_print("warning: port 1 test failed!\n");
     }
+
+    // Check if the 'disable second ps2 port' bit is set, if not, we know that this controller doesn't support a second ps2 port because it didn't disable it at step 1
+    if (config & 0b00010000)
+    {
+        // TODO check if secondary port is supported by enabling and disabling the secondary port
+
+        keyboard_wait_can_write();
+
+        // Test ps2 port 2
+        port_out8(KEYBOARD_COMMAND_REGISTER, 0xA9);
+
+        keyboard_wait_can_read();
+
+        unsigned char port2_test_result = port_in8(KEYBOARD_DATA_REGISTER);
+        if (port2_test_result != 0)
+        {
+            console_print("warning: port 2 test failed!\n");
+        }
+    }
+
+    console_print("ps2 controller initialized\n");
 }
