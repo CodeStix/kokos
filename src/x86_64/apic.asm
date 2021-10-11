8b9bb92478cdb877452c2693b4f96cd789f1376d
Stijn Rogiest <reddusted@gmail.com>
2021-10-11 18:10:56 +0200

    Disable old PICs and relocate vectors

--- src/x86_64/apic.asm
+++ src/x86_64/apic.asm
@@ -4,12 +4,64 @@ global apic_check_supported
 section .text
 bits 64
 
-; TODO check apic support using acpi MADT table
-
+; Because we will use the newer APIC (advanced programmable interrupt controller) instead of the old PICs
+; we need to disable the old PIC. Because PIC's can generate random interrupts (by electric noise ect), we must also offset these interrupts to an unused
+; region of interrupt vectors, so it does not trigger random system interrupts (worse: exception interrupts)
 apic_disable_pic:
+    mov al, 0x11            ; Start master PIC initialization sequence, it now expects 3 bytes at port 0x21 to configure this PIC
+    out 0x20, al          
+
+    mov al, 0               ; Port 80 is unused by the computer and is used to simulate a delay, because it could take some time before the PIC answers
+    out 0x80, al             
+
+    mov al, 0xFF - 16
+    out 0x21, al            ; Set master interrupt vectors to be 0xFF - 16 -> 0xFF - 8
+
+    mov al, 0
+    out 0x80, al
+    
+    mov al,  0b00000100     ; Tell master PIC that there is a slave PIC at IRQ2 pin
+    out 0x21, al            
+
+    mov al, 0
+    out 0x80, al
+
+    mov al, 0x01            ; PIC mode 1 (?)
+    out 0x21, al          
+
+    mov al, 0
+    out 0x80, al
+
+
+    mov al, 0x11            ; Start slave PIC initialization sequence, it now expects 3 bytes at port 0x21 to configure this PIC
+    out 0xa0, al          
+
+    mov al, 0
+    out 0x80, al
+
+    mov al, 0xFF - 8        ; Set slave interrupt vectors to be 0xFF - 8 -> 0xFF (each PIC has 8 interrupts)
+    out 0xa1, al      
+
+    mov al, 0
+    out 0x80, al
+
+    mov al, 0b00000010      ; Tell slave PIC that it is a slave
+    out 0xa1, al    
+
+    mov al, 0
+    out 0x80, al
+
+    mov al, 0x01            ; PIC mode 1 (?) 
+    out 0xa1, al          
+
+    mov al, 0
+    out 0x80, al
+
+
     mov al, 0xff
-    out 0xa1, al
-    out 0x21, al
+    out 0xa1, al            ; Disable slave pic (by masking all interrupts using 0xff)
+    out 0x21, al            ; Disable master pic
+
     ret
 
 apic_check_supported:
